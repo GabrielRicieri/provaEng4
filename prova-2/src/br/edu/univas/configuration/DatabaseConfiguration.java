1398ef44213e7bc3543cc7e2d7ccabd65974e60b
gabriel <gabrielricieri134@gmail.com>
2021-11-30 21:33:36 -0300

    prova 2 final

--- prova-2/src/br/edu/univas/configuration/DatabaseConfiguration.java
+++ prova-2/src/br/edu/univas/configuration/DatabaseConfiguration.java
@@ -14,6 +14,7 @@ public class DatabaseConfiguration {
 	public static DatabaseConfiguration getInstance() {
 		if (DataBaseinstance == null) {
 			DataBaseinstance = new DatabaseConfiguration();
+			return DataBaseinstance;
 		}else{
 			return DataBaseinstance;
 		}
